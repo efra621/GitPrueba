ffb52d709121a80bf43221b5b36b87251dd5e6df
Efrain <eframorales421@gmail.com>
2021-08-01 05:28:21 -0300

    Otra rama

--- app/src/main/java/app/gratumain/gitprueba/MainActivity.kt
+++ app/src/main/java/app/gratumain/gitprueba/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 
 //Primer comentario
 //Segundo comentario
+//git checkout -b cambiosAplicados
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
