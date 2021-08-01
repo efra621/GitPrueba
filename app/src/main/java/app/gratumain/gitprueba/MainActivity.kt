9af26d264004359e2eca072c06cd7bf6626c86a5
Efrain <eframorales421@gmail.com>
2021-08-01 05:22:59 -0300

    "Segundo commit"

--- app/src/main/java/app/gratumain/gitprueba/MainActivity.kt
+++ app/src/main/java/app/gratumain/gitprueba/MainActivity.kt
@@ -3,6 +3,8 @@ package app.gratumain.gitprueba
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+//Primer comentario
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
